906e69f31d381ecb6f9605ede6415ffa14bf5fcb
km796 <31836577+km796@users.noreply.github.com>
2022-04-04 12:09:07 +0900

    Stock info fix

--- StockTaxSaver/StockTaxSaver/Network/APIService.swift
+++ StockTaxSaver/StockTaxSaver/Network/APIService.swift
@@ -54,26 +54,6 @@ struct APIService {
         }.resume()
         
     }
-    
-    func fetchStockInfo(with symbol: String, completion: @escaping (Result<StockInfo, NetworkError>) -> Void) {
-        guard let url = URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=\(symbol)&interval=5min&apikey=\(API_KEY)") else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(.failure(.requestFailed))
-                return
-            }
-            do {
-                let stockInfo = try JSONDecoder().decode(StockInfo.self, from: data)
-                completion(.success(stockInfo))
-            } catch {
-                completion(.failure(.decodingFailed))
-            }
-        }.resume()
-    }
 }
 
 
