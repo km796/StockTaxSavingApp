ea80f9bd3b8fda1d707be71f53fb6503766b467e
km796 <31836577+km796@users.noreply.github.com>
2022-05-17 16:11:16 +0900

    Currency container added

---
+++ StockTaxSaver/StockTaxSaver/Model/CurrencyExchange.swift
@@ -0,0 +1,12 @@
+//
+//  CurrencyExchange.swift
+//  StockTaxSaver
+//
+//  Created by Min Su Kang on 2022/05/09.
+//
+
+import Foundation
+
+struct CurrencyExchange: Decodable{
+    let basePrice: Double
+}
